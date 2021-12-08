30635b4f5f857135cb0db16e24a51b06ac1fca2e
Paul Bellamy <paul@stellar.org>
2021-12-08 15:52:15 -0800

    yolo parallelizing ingest processors
    No idea if this works right or not. Trying it. Need to re-read the
    processors closer.
    
    Left committing as serial.

--- services/horizon/internal/ingest/group_processors.go
+++ services/horizon/internal/ingest/group_processors.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stellar/go/ingest"
 	"github.com/stellar/go/support/errors"
+	"golang.org/x/sync/errgroup"
 )
 
 type processorsRunDurations map[string]time.Duration
@@ -28,14 +29,18 @@ func newGroupChangeProcessors(processors []horizonChangeProcessor) *groupChangeP
 }
 
 func (g groupChangeProcessors) ProcessChange(ctx context.Context, change ingest.Change) error {
+	err, ctx := errgroup.WithContext(ctx)
 	for _, p := range g.processors {
-		startTime := time.Now()
-		if err := p.ProcessChange(ctx, change); err != nil {
-			return errors.Wrapf(err, "error in %T.ProcessChange", p)
-		}
-		g.AddRunDuration(fmt.Sprintf("%T", p), startTime)
+		err.Go(func() error {
+			startTime := time.Now()
+			if err := p.ProcessChange(ctx, change); err != nil {
+				return errors.Wrapf(err, "error in %T.ProcessChange", p)
+			}
+			g.AddRunDuration(fmt.Sprintf("%T", p), startTime)
+			return nil
+		})
 	}
-	return nil
+	return err.Wait()
 }
 
 func (g groupChangeProcessors) Commit(ctx context.Context) error {
@@ -62,14 +67,18 @@ func newGroupTransactionProcessors(processors []horizonTransactionProcessor) *gr
 }
 
 func (g groupTransactionProcessors) ProcessTransaction(ctx context.Context, tx ingest.LedgerTransaction) error {
+	err, ctx := errgroup.WithContext(ctx)
 	for _, p := range g.processors {
-		startTime := time.Now()
-		if err := p.ProcessTransaction(ctx, tx); err != nil {
-			return errors.Wrapf(err, "error in %T.ProcessTransaction", p)
-		}
-		g.AddRunDuration(fmt.Sprintf("%T", p), startTime)
+		err.Go(func() error {
+			startTime := time.Now()
+			if err := p.ProcessTransaction(ctx, tx); err != nil {
+				return errors.Wrapf(err, "error in %T.ProcessTransaction", p)
+			}
+			g.AddRunDuration(fmt.Sprintf("%T", p), startTime)
+			return nil
+		})
 	}
-	return nil
+	return err.Wait()
 }
 
 func (g groupTransactionProcessors) Commit(ctx context.Context) error {
